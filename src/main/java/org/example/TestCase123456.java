068cc14605e82c8d009b9b16e767b50f8d882ca9
elvindanyarov@gmail.com <Denyarov_91>
2023-01-15 18:50:06 -0500

    1st commit 1st package

---
+++ src/main/java/org/example/TestCase123456.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class TestCase123456 {
+    public static void main(String[] args) {
+        System.out.println("First Print");
+    }
+}
--- src/main/java/org/example/firstPackage/javaClass.java
+++
@@ -1,7 +0,0 @@
-package org.example.firstPackage;
-
-public class javaClass {
-    public static void main(String[] args) {
-        System.out.printf("1st print");
-    }
-}
